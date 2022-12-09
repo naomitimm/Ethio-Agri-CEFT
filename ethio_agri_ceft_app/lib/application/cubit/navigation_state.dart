399c5370f39e1b69861eb517c207195f5ca858bd
Naomi Timothy <naomitim17.com>
2022-12-09 20:27:47 +0300

    navigation setup

--- ethio_agri_ceft_app/lib/application/cubit/navigation_state.dart
+++ ethio_agri_ceft_app/lib/application/cubit/navigation_state.dart
@@ -1,17 +1,31 @@
 part of 'navigation_cubit.dart';
 
 @immutable
-abstract class NavigationState {}
+abstract class NavigationState extends Equatable {}
 
-class NavigationInitial extends NavigationState {}
+class NavigationInitial extends NavigationState {
+  @override
+  List<Object?> get props => [];
+}
 
-class LandingRoute extends NavigationState {}
+class LandingRoute extends NavigationState {
+  @override
+  List<Object?> get props => [];
+}
 
-class LoginRoute extends NavigationState {}
+class LoginRoute extends NavigationState {
+  @override
+  List<Object?> get props => [];
+}
 
-class SignupRoute extends NavigationState {}
+class SignupRoute extends NavigationState {
+  @override
+  List<Object?> get props => [];
+}
 
 class DashboardRoute extends NavigationState {
   final int tabIndex;
   DashboardRoute({required this.tabIndex});
+  @override
+  List<Object?> get props => [tabIndex];
 }
