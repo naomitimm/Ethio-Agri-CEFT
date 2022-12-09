ddf7d59ae48fab01c8ec5f3c5d30989cba253a1d
Naomi Timothy <naomitim17.com>
2022-12-09 20:27:40 +0300

    navigation set up

--- ethio_agri_ceft_app/lib/presentation/exports.dart
+++ ethio_agri_ceft_app/lib/presentation/exports.dart
@@ -1,2 +1,10 @@
 // packages
 export 'package:flutter/material.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:ethio_agri_ceft_app/application/cubit/navigation_cubit.dart';
+
+// screens
+export 'package:ethio_agri_ceft_app/presentation/auth/landing_screen.dart';
+export 'package:ethio_agri_ceft_app/presentation/auth/login_screen.dart';
+export 'package:ethio_agri_ceft_app/presentation/auth/signup_screen.dart';
+export 'package:ethio_agri_ceft_app/presentation/dashboard/dashboard_screen.dart';
